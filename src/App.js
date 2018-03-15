ec16ac589ff53904a1153e9f57c609d4a998c82f
Tom Snively <voxbop@gmail.com>
2018-03-15 14:06:46 -0400

    Got the inputs to display. (Have to move the methods and a few other things.)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
+import Inputs from './Inputs';
 
 class App extends Component {
   render() {
     return (
         <div className='container'>
             <Header />
+            <Inputs />
             <h1>Hello React</h1>
         </div>
     );
--- src/Inputs.js
+++ src/Inputs.js
@@ -5,38 +5,35 @@ class Inputs extends Component {
         return (
             <div className="panel panel-default">
                 <p>Choose the grid size; resizing will start over.
-                    <label class="radio-inline">
-                        <input type="radio" name="gridSize" value="3" onclick="sizeChecked(3)"> 3x3
+                    <label className="radio-inline">
+                        <input type="radio" name="gridSize" value="3" onclick="sizeChecked(3)" /> 3x3
                     </label>
-                    <label class="radio-inline">
-                        <input type="radio" name="gridSize" value="5" onclick="sizeChecked(5)"> 5x5
+                    <label className="radio-inline">
+                        <input type="radio" name="gridSize" value="5" onclick="sizeChecked(5)" /> 5x5
                     </label>
-                    <label class="radio-inline">
-                        <input type="radio" name="gridSize" value="7" onclick="sizeChecked(7)"> 7x7
+                    <label className="radio-inline">
+                        <input type="radio" name="gridSize" value="7" onclick="sizeChecked(7)" /> 7x7
                     </label>
-                    <label class="radio-inline">
-                        <input type="radio" name="gridSize" value="11" checked="checked" onclick="sizeChecked(11)"> 11x11
+                    <label className="radio-inline">
+                        <input type="radio" name="gridSize" value="11" checked="checked" onclick="sizeChecked(11)" /> 11x11
                     </label>
-                    <label class="radio-inline">
-                        <input type="radio" name="gridSize" value="13" onclick="sizeChecked(13)"> 13x13
+                    <label className="radio-inline">
+                        <input type="radio" name="gridSize" value="13" onclick="sizeChecked(13)" /> 13x13
                     </label>
-                    <label class="radio-inline">
-                        <input type="radio" name="gridSize" value="17" onclick="sizeChecked(17)"> 17x17
+                    <label className="radio-inline">
+                        <input type="radio" name="gridSize" value="17" onclick="sizeChecked(17)" /> 17x17
                     </label>
                 </p>
                 <p>Choose the heat interval speed. How many times does the screen update per second (1-20)?
-                    <input type="number" id="intervalSpeed" min="1" max="20" value="5" onchange="speedChanged()"/>
+                    <input type="number" id="intervalSpeed" min="1" max="20" value="5" onchange="speedChanged()" />
                 </p>
                 <p>
-                    <input type="button" class="btn" value="Turn All On" onclick="turnAllOn()" />
-                    <input type="button" class="btn" value="Turn All Off" onclick="turnAllOff()" />
+                    <input type="button" className="btn" value="Turn All On" onclick="turnAllOn()" />
+                    <input type="button" className="btn" value="Turn All Off" onclick="turnAllOff()" />
                 </p>
-
-
             </div>
         );
     }
 }
 
 export default Inputs;
-
