45843e6410923f3063be91d6512371dae8987370
Tom Snively <voxbop@gmail.com>
2018-04-24 13:27:37 -0400

    Adding trailing 0 in Total Heat

--- src/TotalHeat.js
+++ src/TotalHeat.js
@@ -5,7 +5,8 @@ class TotalHeat extends Component {
     render() {
         return (
             <div className="totalHeat">
-                Total Heat: {this.props.totalHeat}
+                Total Heat: {this.props.totalHeat.toLocaleString(undefined,
+                {'minimumFractionDigits':1,'maximumFractionDigits':1})}
             </div>
         );
     }
